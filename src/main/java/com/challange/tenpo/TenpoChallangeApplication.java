06dc4c00f189427dac362ff8d40249a12fd87d4a
MartinFernandoAndres <martinfernandoandres1996@gmail.com>
2022-06-17 12:31:18 -0300

    Init commit

---
+++ src/main/java/com/challange/tenpo/TenpoChallangeApplication.java
@@ -0,0 +1,13 @@
+package com.challange.tenpo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TenpoChallangeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TenpoChallangeApplication.class, args);
+    }
+
+}
